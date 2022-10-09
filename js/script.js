0bbf1d8aa373bd965848370d2e8e67a8896b07d3
Gean J. Silva <109292751+GeanJSilva@users.noreply.github.com>
2022-10-09 18:33:07 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,23 +1,34 @@
 $(document).ready(function() {
-  let slideIndex = 1;
-  showSlides();
-  
-  function showSlides() {
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
-    let dots = document.getElementsByClassName("dot");
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
+    
+    let slideIndex = 1;
+    showSlides();
+    
+    //-------Carrossel---------
+    
+    function showSlides() {
+        let i;
+        let slides = document.getElementsByClassName("index-slides");
+        let dots = document.getElementsByClassName("index-ponto");
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";  
+        }
+        
+        slideIndex++;
+        
+        if (slideIndex > slides.length) {
+            slideIndex = 1
+        }    
+        
+        for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+        }
+        
+        slides[slideIndex-1].style.display = "block";  
+        dots[slideIndex-1].className += " active";
+        setTimeout(showSlides, 3000); // mudar a cada 2 segundos
     }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}    
-    for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";  
-    dots[slideIndex-1].className += " active";
-    setTimeout(showSlides, 2000); // Change image every 2 seconds
-  }
-  
+
+
+
 
 })
